5e6c3215e9454025cf0bd774ad3025ad1a05c15d
Jorge O. Castro <jorge.castro@gmail.com>
2023-08-31 11:19:08 -0400

    Update content.ts

--- src/content.ts
+++ src/content.ts
@@ -22,9 +22,9 @@ export const LangUsersListItems = [
   'Zero maintenance',
   'Included GPU drivers',
 ]
-export const LangUsersAppendix = 'Updates are image-based and automatic. Applications are logically separated from the system by using Flatpaks and combined with native container workflows to provide a generational leap in reliability and composability. 
+export const LangUsersAppendix = `Updates are image-based and automatic. Applications are logically separated from the system by using Flatpaks and combined with native container workflows to provide a generational leap in reliability and composability. 
   
-Images are available for your PC, M1/M2 Macs, and Framework laptops.'
+Images are available for your PC, M1/M2 Macs, and Framework laptops.`
 
 //
 // Developers Section
