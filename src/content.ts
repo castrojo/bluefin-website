c046d4fd1f79a14c49636706981cd15efe512c9a
Jorge O. Castro <jorge.castro@gmail.com>
2023-09-04 14:00:52 -0400

    Update content.ts

--- src/content.ts
+++ src/content.ts
@@ -148,6 +148,7 @@ export const LangFooterReferences = `All artwork built by humans.
 - Logos and Wallpapers - [Jacob Schnurr](https://www.etsy.com/listing/1425657775/cretaceous-chonkers-chonky-dinosaur) 
 - Bluefin Wallpaper - Andy Frazer, [DragonsofWales](https://www.etsy.com/fi-en/shop/DragonsofWales?ref=profile_header)
 - Website - [Jan Dolanský](https://dolansky.dev/)
+- Technical Leads - Marco Ceppi and Brian Ketelsen
 - Project Management - Aaron Lake 
 - Product Management - Dustin Kirkland`
 
