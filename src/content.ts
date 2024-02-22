dcb009c99482ecf4769ec902a2a4b18b9d3bc9ff
Kyle Gospodnetich <me@kylegospodneti.ch>
2024-02-21 16:20:59 -0800

    Typo fix

--- src/content.ts
+++ src/content.ts
@@ -94,7 +94,7 @@ She represents the state of the art … a fragile, beautiful, and unique creatur
 `
 
 // Youtube video > share > embed > copy and paste the text inside the string
-export const LangAppendixTYVideo = '<iframe width="560" height="315" src="https://www.youtube.com/embed/Nz-yyDwTfRM&autoplay=1" srcdoc="<style>*{padding:0;margin:0;overflow:hidden}html,body{height:100%}img,span{position:absolute;width:100%;top:0;bottom:0;margin:auto}span{height:1.5em;text-align:center;font:48px/1.5 sans-serif;color:white;text-shadow:0 0 0.5em black}</style><a href=https://www.youtube.com/embed/Nz-yyDwTfRM?autoplay=1><img src=https://img.youtube.com/vi/Nz-yyDwTfRM/hqdefault.jpg alt=\'Bluefin Linux introduction\'><span>▶</span></a>" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen title="Bluefin Linux introduction"></iframe>'
+export const LangAppendixYTVideo = '<iframe width="560" height="315" src="https://www.youtube.com/embed/Nz-yyDwTfRM&autoplay=1" srcdoc="<style>*{padding:0;margin:0;overflow:hidden}html,body{height:100%}img,span{position:absolute;width:100%;top:0;bottom:0;margin:auto}span{height:1.5em;text-align:center;font:48px/1.5 sans-serif;color:white;text-shadow:0 0 0.5em black}</style><a href=https://www.youtube.com/embed/Nz-yyDwTfRM?autoplay=1><img src=https://img.youtube.com/vi/Nz-yyDwTfRM/hqdefault.jpg alt=\'Bluefin Linux introduction\'><span>▶</span></a>" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen title="Bluefin Linux introduction"></iframe>'
 
 //
 //
