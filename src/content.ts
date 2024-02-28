b2d46f783a407e6a1072f29c87a203946b16fc0a
Jorge O. Castro <jorge.castro@gmail.com>
2024-02-28 10:36:35 -0500

    Update content.ts

--- src/content.ts
+++ src/content.ts
@@ -48,10 +48,8 @@ export const LangDevsText = 'Container focused workflows to get you started depe
 
 export const LangDevsBoxOne = 'Visual Studio Code with devcontainers.'
 export const LangDevsBoxTwo = 'Homebrew on-tap'
-export const LangDevsTowerImageURL = './characters/devs.webp'
 export const LangDevsBoxThree = 'Devpod - developer environments as code'
-export const LangDevsBoxFour = 'Any distribution container'
-export const LangDevsTowerImageURL = './brands/dontrememberthisbrand.svg'
+export const LangDevsBoxFour = 'Ptyxis - Container focused terminal'
 export const LangDevsAppendix = `The inclusion of a [container runtime](https://podman.io) unlocks your team\'s potential. Like most dromaeosaurs, she knows how to ship to production. Bluefin is designed to be [forked and extended](https://universal-blue.discourse.group/docs?topic=43) depending on your requirements, using standard cloud-native tools and techniques. 
 
 We target Linux operators who don't use Linux on the desktop because it has failed them. So we're taking the Linux desktop through its cloud-native journey. There will be dinosaurs on this trip.
