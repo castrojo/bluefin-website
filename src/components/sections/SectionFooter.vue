ad603d542e6291e5a98b8f333597d75b90591513
Kyle Gospodnetich <me@kylegospodneti.ch>
2024-03-30 20:48:09 -0700

    Move CSS to scss, fix mobile

--- src/components/sections/SectionFooter.vue
+++ src/components/sections/SectionFooter.vue
@@ -5,11 +5,11 @@ import { LangFooterProject, LangFooterProjectTitle, LangFooterReferences, LangPo
 
 <template>
   <footer>
-    <section id="contributors" class="section-wrap" style="min-height:0px">
-      <div class="container" style="display:block;width:100%;text-align:center;padding:0px 0px 40px 0px;">
-        <h2 style="font-size:42px;margin-bottom:5px;">Our Team</h2>
+    <section id="contributors" class="section-wrap">
+      <div class="container">
+        <h2>Our Team</h2>
         <p style="font-size:18px;">Bluefin is built by a dedicated group of maintainers and contributors.</p>
-        <iframe style="width:100%;margin-top:30px;" id="contributor-container" src="/contributors.html"></iframe>
+        <iframe id="contributor-container" src="/contributors.html"></iframe>
       </div>
     </section>
     <div class="container">
