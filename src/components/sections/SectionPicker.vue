205e9fff4265fe245d1e8a21a6fead2fa8fe9a06
Jorge O. Castro <jorge.castro@gmail.com>
2024-04-23 12:24:15 -0400

    Update SectionPicker.vue

--- src/components/sections/SectionPicker.vue
+++ src/components/sections/SectionPicker.vue
@@ -6,7 +6,7 @@ import SceneVisibilityChecker from '../common/SceneVisibilityChecker.vue'
   <section id="scene-picker" class="section-wrap">
     <div class="container">
       <div>
-        <h2>Try Bluefin (Beta)</h2>
+        <h2>Try Bluefin</h2>
         <p>Use the form below to get the correct ISO download link for your hardware and use case.</p>
       </div>
       <iframe id="image-picker" src="/image-picker.html" loading="lazy"></iframe>
