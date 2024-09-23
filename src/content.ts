27fbd606d69bdb02200eba7140c4bb18b3d8cfb1
Jorge O. Castro <jorge.castro@gmail.com>
2024-09-23 09:54:41 -0400

    Update content.ts

--- src/content.ts
+++ src/content.ts
@@ -25,7 +25,7 @@ export const LangUsersBluefinImageURL = './characters/bluefin-small.webp'
 export const LangUsersAppendix = `
 Featuring automatic image-based updates and a simple graphical application store, Bluefin is designed to get out of your way. Get what you want without sacrificing system stability. The Linux client has evolved, a clean start for the next generation. Check the [announcement blog post](https://www.ypsidanger.com/announcing-project-bluefin/) and [introductory documentation](https://docs.projectbluefin.io) for more information. 
 
-Images are available for PC, Framework and ASUS laptops, and Microsoft Surface devices. M1/M2 Mac Support coming soon. 
+Images are available for PC, Framework and ASUS laptops, and Microsoft Surface devices. 
 `
 
 //
