ca4bb78bc72343b0b8717202be8fcf3d3970fc88
Jorge O. Castro <jorge.castro@gmail.com>
2024-11-03 08:46:32 -0500

    Update SectionPicker.vue
    Fixes #33

--- src/components/sections/SectionPicker.vue
+++ src/components/sections/SectionPicker.vue
@@ -7,7 +7,7 @@ import SceneVisibilityChecker from '../common/SceneVisibilityChecker.vue'
     <div class="container">
       <div>
         <h2>Try Bluefin</h2>
-        <p>Use the form below to get the correct ISO download link for your hardware and use case. The team recommends <a href="https://flathub.org/apps/org.fedoraproject.MediaWriter">Fedora Media Writer</a> or <a href="https://www.ventoy.net/en/index.html">Ventoy</a> to create installation media:</p>
+        <p>Use the form below to get the correct ISO download link for your hardware and use case. The team recommends <a href="https://flathub.org/apps/org.fedoraproject.MediaWriter">Fedora Media Writer</a> to create installation media:</p>
       </div>
       <iframe id="image-picker" src="/image-picker.html" loading="lazy"></iframe>
     </div>
