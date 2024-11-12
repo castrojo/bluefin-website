2ed03ec7465d961734422ae5ecdbf6a432ac23f4
Tulip Blossom <tulilirockz@outlook.com>
2024-11-12 10:42:15 -0300

    feat: automatic browser locale detection

--- src/App.vue
+++ src/App.vue
@@ -77,6 +77,9 @@ import { i18n } from "./locales/schema"
 let urlParams = new URLSearchParams(window.location.search)
 if (urlParams.has("lang")) {
   ;(i18n.global as any).locale = urlParams.get("lang") ?? "en-US"
+} else {
+  let lang = window.navigator.userLanguage || window.navigator.language
+  ;(i18n.global as any).locale = lang ?? "en-US";
 }
 
 import { useI18n } from "vue-i18n"
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,9 @@
+declare module '*.vue';
+
 import { createApp } from 'vue'
 import App from './App.vue'
 import './style/index.scss'
 import { i18n } from './locales/schema';
-
-let urlParams = new URLSearchParams(window.location.search);
-if (urlParams.has('lang')) {
-  let locale = urlParams.get('lang') ?? "en-us"
-  i18n.global.locale = locale as "en-US" | "pt-BR";
-}
 
 const app = createApp(App)
 app.use(i18n)
