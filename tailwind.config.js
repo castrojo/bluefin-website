885938e93291a0b38c4a61b744d62c0c3d0836b0
James Reilly <jreilly1821@gmail.com>
2025-01-23 21:40:47 +0530

    Updated Vite, Vue, and installled tailwind via Vite

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-  content: [],
+  content: ['./index.html',"./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     fontFamily: {
       inter: ["Inter"], 
     },
     extend: {
-    
     },
   },
   plugins: [],
 }
-
