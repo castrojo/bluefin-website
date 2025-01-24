a6ae07f3cf7de851b6442aa2c8ca40a030c59a5c
Jorge O. Castro <jorge.castro@gmail.com>
2025-01-24 08:51:39 -0500

    Revert "Update Tailwind CSS configuration and dependencies"

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  content: [],
   theme: {
     fontFamily: {
       inter: ["Inter"], 
     },
     extend: {
+    
     },
   },
   plugins: [],
 }
+
